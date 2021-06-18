05d49d4c67870b8c005755557a2431bdcb166cd7
wang <awd1232009@gmail.com>
2021-06-18 16:03:21 +0800

    feat: adding crowdsourcing plantform

---
+++ pkg/crowdsourcing/plantform/plantform.go
@@ -0,0 +1,73 @@
+package plantform
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/wang12d/Go-Crowdsourcing-DApp/pkg/crowdsourcing/task"
+)
+
+type plantform struct {
+	workers     int         // The number of workers in the Plantform
+	requesters  int         // The number of requesters in the Plantform
+	keyIndex    int         // Key index used to get the private key
+	tasks       []task.Task // Tasks available in the Plantform
+	privateKeys []string    // All of private keys
+}
+
+const (
+	numberOfAccount = 60
+)
+
+var (
+	CP   *plantform
+	once sync.Once
+)
+
+func init() {
+	once.Do(func() {
+		CP = &plantform{
+			keyIndex:    0,
+			workers:     0,
+			requesters:  0,
+			tasks:       make([]task.Task, 0),
+			privateKeys: make([]string, numberOfAccount),
+		}
+	})
+
+	// Parser accounts file
+	pwd, _ := os.Getwd()
+	accountFile := filepath.Join(pwd, "pkg", "crowdsourcing", "plantform", "accounts.json")
+	accountFileHandler, err := os.Open(accountFile)
+	if err != nil {
+		log.Fatalf("Read account file error: %v\n", err)
+	}
+	defer accountFileHandler.Close()
+
+	accountData, _ := ioutil.ReadAll(accountFileHandler)
+
+	var accounts map[string]interface{}
+
+	json.Unmarshal([]byte(accountData), &accounts)
+
+	switch acc := accounts["private_keys"].(type) {
+	case map[string]interface{}:
+		cnt := 0
+		for _, val := range acc {
+			CP.privateKeys[cnt] = val.(string)
+			cnt++
+		}
+	default:
+		log.Fatalf("Json parser error, cannot get private keys.")
+	}
+
+}
+
+func (cp *plantform) RegisterWorker() {
+	fmt.Printf("private key %v", cp.privateKeys[cp.keyIndex])
+}
