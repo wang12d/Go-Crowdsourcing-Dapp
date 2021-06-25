d2a119913238178b3a5d659c166fe2d660456813
heng <awd1232009@gmail.com>
2021-06-25 22:07:06 +0800

    feat: adding encryption and decryption to worker and requester

---
+++ pkg/crowdsourcing/utils/cryptograph/padding.go
@@ -0,0 +1,14 @@
+package cryptograph
+
+import "bytes"
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
